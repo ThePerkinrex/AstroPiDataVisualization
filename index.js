4750195995fabb3cbfa7f9abba15dd1fabb65f2f
theperkinrex <theperkinrex@gmail.com>
2018-11-27 20:02:35 +0100

    Add time frame to graph & beautify it

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ let height = 815;
 let graphHeight = height-200;
 let graphXStart = 40;
 let graphYStart = 20;
-let graphXEnd = width-5;
+let graphXEnd = width-10;
 let graphYEnd = graphHeight-20;
 
 let graphInnerHeight = graphYEnd-graphYStart;
 let graphInnerWidth = graphXEnd-graphXStart;
-
-
 
 
 
@@ -32,35 +30,50 @@ function preload(){
 
 
 // Here put the maximum & minimum of all the columns used
-let max_data = 0.042;
-let min_data = -0.026;
+let max_data = 0.042; // example:  0.042
+let min_data = -0.026; // example: -0.026
 
 // Here put the number of columns used
-let nOfElements = 3;
+let nOfElements = 3; // example: 3
+
+// Here put the hours the experiment run for (for AstroPi, it is 3h)
+let hoursRun = 3;
+// Here put how often do you want a line appearing (default is 5min)
+let minSep = 5;
+
+// DO NOT CHANGE ----------------------------------+
+graphColumnsUnits = new Array(nOfElements); //     |
+// END DO NOT CHANGE ------------------------------+
+
+
 
 function setup(){
     // Here put the name of the columns you want to select
-    graphColumns = ['accel_x', 'accel_y', 'accel_z'];
+    graphColumns = ['accel_x', 'accel_y', 'accel_z']; // example: ['accel_x', 'accel_y', 'accel_z']
     // Here put the units if you want, of the columns
     // If you only want to leave out one unit, write undefined where that unit would be
-    graphColumnsUnits = ['Gs', 'Gs', 'Gs'];
+    graphColumnsUnits = ['Gs', 'Gs', 'Gs']; // example: ['Gs', 'Gs', 'Gs']
     // Here put the colors of the graph lines in rgb
-    graphColors = [color(255, 130, 0), color(26, 165, 16), color(14, 105, 165)];
+    graphColors = [color(255, 130, 0), color(26, 165, 16), color(14, 105, 165)]; // example: [color(255, 130, 0), color(26, 165, 16), color(14, 105, 165)]
     // Custom END ---------------------------------------------------------------
 
 
     lineColor = color(180);
-    boldLineColor = color(100)
+    boldLineColor = color(100);
+    
+    
 
-    createCanvas(width, height);
-    sep = graphInnerWidth/table.rows.length;
-    //console.log(sep);
-    textSize(15);
-    console.log('Loaded');
-
-    if(graphColumns.length != nOfElements && graphColumnsUnits.length != nOfElements && graphColors.length != nOfElements)
+    if(graphColumns.length < nOfElements || graphColumnsUnits.length < nOfElements || graphColors.length < nOfElements){
+        createP('ERROR: Column data is not the length it should be');
         throw Error('Column data is not the length it should be')
-    text('Loaded data! Creating graph ...', 0,15);
+    }else{
+        createCanvas(width, height);
+        sep = graphInnerWidth/table.rows.length;
+        //console.log(sep);
+        textSize(15);
+        console.log('Loaded');
+        text('Loaded data! Creating graph ...', 0,15);
+    }
 }
 
 let first_time = true;
@@ -77,13 +90,14 @@ function draw(){
         background(255);
         noFill();
         stroke(lineColor);
-        line(graphXStart, graphYEnd, graphXEnd, graphYEnd);
-        for(let i = graphXStart; i <= graphXEnd;i+=(graphInnerWidth/((60/5)*3))){
+        for(let i = graphXStart; i <= graphXEnd;i+=(graphInnerWidth/((60/minSep)*hoursRun))){
             line(i, graphYStart, i, graphYEnd);
         }
         stroke(boldLineColor);
         strokeWeight(3);
-        for(let x = graphXStart; x <= graphXEnd; x += graphInnerWidth/3){
+        strokeCap(PROJECT);
+        line(graphXStart, graphYEnd, graphXEnd, graphYEnd);
+        for(let x = graphXStart; x <= graphXEnd; x += graphInnerWidth/hoursRun){
             line(x, graphYStart, x, graphYEnd);
         }
         if(zeroMarkHeight)
@@ -101,10 +115,15 @@ function draw(){
         fill(boldLineColor);
         let dataTextSize = 12;
         textSize(dataTextSize);
-        text(max_data, 0, graphYStart + dataTextSize/2);
-        text(min_data, 0, graphYEnd + dataTextSize/2);
+        text(max_data, graphXStart-textWidth(max_data + ' '), graphYStart + dataTextSize/2);
+        text(min_data, graphXStart-textWidth(min_data + ' '), graphYEnd + dataTextSize/2);
         if(zeroMarkHeight)
-            text('0.0', 0, zeroMarkHeight + dataTextSize/2);
+            text('0.0', graphXStart-textWidth('0.0 '), zeroMarkHeight + dataTextSize/2);
+        
+        for(let i = 0; i <= hoursRun; i++){
+            if(i>0)
+            text(i+'h', graphXStart+graphInnerWidth*i/hoursRun-(textWidth(i+'h')/2), graphYEnd+dataTextSize);
+        }
         
         noFill();
         let x = graphXStart;
